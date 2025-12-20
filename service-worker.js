0b43b9c283bbb80c927f516475946c52b57a366e
aitof-stack <aitof@mail.ru>
2025-12-20 09:06:28 +0500

    service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,15 +1,69 @@
 const CACHE_NAME = 'repair-journal-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './login.html',
+  './style.css',
+  './javascript.js',
+  './auth.js'
+];
 
+// Установка Service Worker
 self.addEventListener('install', event => {
-  console.log('Service Worker установлен');
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Кэширование файлов');
+        return cache.addAll(urlsToCache);
+      })
+  );
 });
 
+// Активация Service Worker
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            console.log('Удаление старого кэша:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Обработка запросов
 self.addEventListener('fetch', event => {
   event.respondWith(
-    fetch(event.request)
-      .catch(() => {
-        console.log('Офлайн режим');
-        return new Response('Вы в офлайн режиме');
+    caches.match(event.request)
+      .then(response => {
+        // Возвращаем кэшированный файл, если есть
+        if (response) {
+          return response;
+        }
+        
+        // Иначе загружаем из сети
+        return fetch(event.request)
+          .then(response => {
+            // Проверяем валидность ответа
+            if (!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+            
+            // Клонируем ответ
+            const responseToCache = response.clone();
+            
+            // Кэшируем новый ресурс
+            caches.open(CACHE_NAME)
+              .then(cache => {
+                cache.put(event.request, responseToCache);
+              });
+            
+            return response;
+          });
       })
   );
 });
