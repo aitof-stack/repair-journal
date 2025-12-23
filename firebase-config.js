5eb0ed334071d2a6e208c794b78087c3a0e4a788
aitof-stack <aitof@mail.ru>
2025-12-23 11:59:15 +0500

    firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,19 @@
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyAdOqQX31vCcj7OXVyNSQX_nRUijAGOVKM",
+    authDomain: "repair-journal-eadf1.firebaseapp.com",
+    projectId: "repair-journal-eadf1",
+    storageBucket: "repair-journal-eadf1.firebasestorage.app",
+    messagingSenderId: "525057868534",
+    appId: "1:525057868534:web:372b03243b0bc34b31e2d7"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+</script>
